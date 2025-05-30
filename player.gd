ce5a54296b9d41a13124dac7c42a22264a737437
martypower2064 <martypower2064@gmail.com>
2025-05-30 12:23:09 +0100

    added all game files :3

---
+++ player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D 
+
+@export var speed = 600
+var bullet_path=preload("res://projectiles/bullet.tscn")
+@onready var eyes: Node2D = $Eyes
+
+func fire():
+	var bullet=bullet_path.instantiate()
+	bullet.dir = rotation
+	bullet.pos = eyes.global_position
+	bullet.rota = global_rotation
+	get_parent().add_child(bullet)
+
+
+
+func get_input():
+	look_at(get_global_mouse_position())
+	var attack = Input.is_action_pressed("click")
+	var input_dir = Input.get_vector("left","right","up","down")
+	velocity = input_dir * speed
+	if attack == true:
+		fire()
+	 
+	
+func _physics_process(delta: float) -> void: 
+	get_input()
+	move_and_slide()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
