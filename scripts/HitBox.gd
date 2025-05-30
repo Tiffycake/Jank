07d55a204555be1be81f5a8917ccac0fc5ee7d0f
martypower2064 <martypower2064@gmail.com>
2025-05-30 18:47:18 +0100

    trying to add hitboxes (very working)

---
+++ scripts/HitBox.gd
@@ -0,0 +1,16 @@
+class_name HitBox
+extends Area2D
+
+@export var damage : int = 10 : set = set_damage, get = get_damage
+
+
+func set_damage(value: int):
+	damage = value
+
+func get_damage() -> int:
+	return damage
+
+
+func _init() -> void:
+	collision_layer = 2
+	collision_mask = 0
