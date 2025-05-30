5c70287c9a048f7bc828a0c5edfe071f324ec9c0
martypower2064 <martypower2064@gmail.com>
2025-05-30 18:46:53 +0100

    trying to add hitboxes (very working)

--- scripts/HurtBox.gd
+++ scripts/HurtBox.gd
@@ -1,11 +1,13 @@
-extends Node
+class_name HurtBox
+extends Area2D
+
+signal recieved_damage(damage: int)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var health:= 1
+	
+	
+func _on_area_entered(hitbox: HitBox) -> void:
+	if hitbox != null:
+		health.health -= hitbox.damage
+		recieved_damage.emit(hitbox.damage)
