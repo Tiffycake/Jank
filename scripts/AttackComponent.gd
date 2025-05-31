5db88a9712550e6804ba92ecbf7d9928d7f52a95
martypower2064 <martypower2064@gmail.com>
2025-05-31 13:25:29 +0100

    idk what im doing :fire:

--- scripts/AttackComponent.gd
+++ scripts/AttackComponent.gd
@@ -3,6 +3,7 @@ class_name AttackComponent
 # this is essentialy a hurbox lol
 @export var attackDamage : int = 5
 
+var attackPosition : Vector2 = Vector2(0,0)
 var poison : int # poison dmg (posion is x dmg for 4 seconds)
 var freeze : int # time frozen 
 var burn : int   # burn time (burn is 3dmg/s)
@@ -22,14 +23,4 @@ func _on_area_entered(area: HitboxComponent) -> void:
 		attack.attackDamage = attackDamage
 		attack.attackPosition = global_position
 		hitbox.damage(attack)
-
-
-func _on_hitbox_area_entered(area: Area2D):
-	if area is HitboxComponent:
-		print(area, "entered")
-		var hitbox : HitboxComponent = area
-		var attack = AttackComponent.new()
-		attack.attackDamage = attackDamage
-		attack.attackPosition = global_position
-		hitbox.damage(attack)
-	
+ 
