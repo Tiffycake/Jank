e2b877bdd030bc40b0a565375bf05cadd3ed5b9b
martypower2064 <martypower2064@gmail.com>
2025-06-01 14:44:18 +0100

    more random stuff :fire:

--- spawner.gd
+++ spawner.gd
@@ -4,14 +4,14 @@ var chest = preload("res://scenes/chest.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
 	self.pressed.connect(spawnerTrigger)
-
+	spawnChest()
 
 func spawnerTrigger() -> void:
+	spawnChest()
+
+
+func spawnChest() -> void:
 	var chest1 = chest.instantiate()
 	chest1.position = position + Vector2(20,136)
 	self.add_child(chest1)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
