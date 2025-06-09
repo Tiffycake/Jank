7c2d17d189503324f0c1fbf095510a2908511d05
loona <loonastarlightb1tch@gmail.com>
2025-06-09 11:14:32 +0100

    ebic menu,,,,,

--- mainMenu.gd
+++ mainMenu.gd
@@ -7,17 +7,24 @@ var playerScene : PackedScene = preload("res://scenes/player.tscn")
 @onready var joinButton := $"joinButton"
 @onready var objectList := $"../objectList"
 
+const buttonScale := Vector2(0.3, 0.3)
+const buttonHoverScale := Vector2(0.32, 0.32)
+
 func hostButtonPressed():
 	print("hostButtonPressed")
 	peer.create_server(port)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(spawnPlayer)
 	spawnPlayer()
+	process_mode = Node.PROCESS_MODE_DISABLED
+	hide()
 
 func joinButtonPressed():
 	print("joinButtonPressed")
 	peer.create_client("localhost",port)
 	multiplayer.multiplayer_peer = peer
+	process_mode = Node.PROCESS_MODE_DISABLED
+	hide()
  
 func spawnPlayer(id = 1):
 	var player = playerScene.instantiate()
@@ -26,8 +33,28 @@ func spawnPlayer(id = 1):
 	objectList.call_deferred("add_child", player)  #mainScene
 	#player.add_child(playerList)
 
+func hostHover():
+	var tween = get_tree().create_tween()
+	tween.tween_property(hostButton, "scale", buttonHoverScale, 0.1)
+
+func joinHover():
+	var tween = get_tree().create_tween()
+	tween.tween_property(joinButton, "scale", buttonHoverScale, 0.1)
+
+func hostReset():
+	var tween = get_tree().create_tween()
+	tween.tween_property(hostButton, "scale", buttonScale, 0.1)
+
+func joinReset():
+	var tween = get_tree().create_tween()
+	tween.tween_property(joinButton, "scale", buttonScale, 0.1)
+
 func _ready() -> void:
 	#connects signals
 	hostButton.pressed.connect(hostButtonPressed)
 	joinButton.pressed.connect(joinButtonPressed)
+	hostButton.mouse_entered.connect(hostHover)
+	joinButton.mouse_entered.connect(joinHover)
+	hostButton.mouse_exited.connect(hostReset)
+	joinButton.mouse_exited.connect(joinReset)
 	#hide()
