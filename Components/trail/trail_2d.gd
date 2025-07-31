ff0cb688f4624a8a27b27be47caf4443a6db451f
loona <loonastarlightb1tch@gmail.com>
2025-07-31 20:07:57 +0100

    trail rewrite

--- Components/trail/trail_2d.gd
+++ Components/trail/trail_2d.gd
@@ -1,16 +1,11 @@
 extends Line2D
 
-var queue : Array
+
 @export var MAX_LENGHT := 10
 
 func _process(delta: float) -> void:
 	var pos = get_parent().global_position
-	queue.push_front(pos)
+	add_point(pos)
 	
-	if queue.size() > MAX_LENGHT:
-		queue.pop_back()
-		
-	clear_points()
-	
-	for point in queue:
-		add_point(point)
+	if get_point_count() > MAX_LENGHT:
+		remove_point(0)
