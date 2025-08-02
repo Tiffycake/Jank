5d6134d5bc978553f785c921cd5395cab2479f54
loona <loonastarlightb1tch@gmail.com>
2025-08-02 14:59:06 +0100

    peak healthbar real and true???

--- Scripts/InventoryUI.gd
+++ Scripts/InventoryUI.gd
@@ -9,11 +9,15 @@ extends Control
 @onready var healthComponent1: HealthComponent = $"../../HealthComponent"
 @onready var healthNum = $"HealthBar/HealthNum"
 @onready var healthbar = $"HealthBar/HealthBar"
+@onready var healthbar_back: TextureProgressBar = $HealthBar/HealthBarBack
 @onready var bullet_count: Label = $AmmoCount/BulletCount
 @onready var bullet_max: Label = $AmmoCount/BulletCount2
 
 
 @onready var ammoCount: = $AmmoCount
+
+var tween_running := false
+
 func _ready() -> void:
 	pass # Replace with function body.
 	
@@ -27,6 +31,18 @@ func updateHealth():
 	healthbar.max_value = healthComponent1.maxHealth
 	healthbar.value = healthComponent1.health
 	healthNum.text = str(healthComponent1.health)+" / "+str(healthComponent1.maxHealth)
+	
+	if healthbar.value != healthbar_back.value and not tween_running:
+		tween_running = true #so true
+		var skibidi = get_tree().create_tween()
+		skibidi.finished.connect(reset_tween)
+		skibidi.set_trans(Tween.TRANS_CIRC) #tramsgener reference... so kewl.,.,,
+		skibidi.set_ease(Tween.EASE_OUT)
+		skibidi.tween_property(healthbar_back, "value", healthbar.value, 0.4)
+		
+#go go my peak reseter
+func reset_tween():
+	tween_running = false
   
 func updateBullet():
 	if !(inventory.selectedNode is Weapon) or inventory.selectedNode == null:
