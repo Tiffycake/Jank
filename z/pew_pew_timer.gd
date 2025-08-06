ca5c476bc43e462b0f07f9581af1243768c6e3b3
loona <loonastarlightb1tch@gmail.com>
2025-08-06 13:08:46 +0100

    silly reload timer text also updates

--- z/pew_pew_timer.gd
+++ z/pew_pew_timer.gd
@@ -4,16 +4,25 @@ class_name PewPewTimer
 @onready var label: Label = $Label
 @onready var texture_progress_bar: TextureProgressBar = $TextureProgressBar
 
+var reloadTime : float
+
 func _ready() -> void:
 	hide()
 
 func startPewPew(a : float):
 	show()
+	reloadTime = a
+	
 	
 	var pewpewtween = get_tree().create_tween() # Tween
 	
-	label.text = str(a) + "s"
-	
 	texture_progress_bar.value = 100
+	pewpewtween.set_parallel()
 	pewpewtween.tween_property(texture_progress_bar,"value",0,a)
+	pewpewtween.tween_property(self, "reloadTime", 0, a)
 	
+	
+
+func _process(delta: float) -> void:
+	if reloadTime != 0: #this makes it only update when needed
+		label.text = str("%0.1f" % reloadTime) + "s"
