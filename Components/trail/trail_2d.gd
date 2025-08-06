00dbb18bd0fb2db0be2e60016ca4603c74c1ea4f
loona <loonastarlightb1tch@gmail.com>
2025-08-06 18:46:57 +0100

    trail should be frame independent now,.,,.

--- Components/trail/trail_2d.gd
+++ Components/trail/trail_2d.gd
@@ -3,9 +3,14 @@ extends Line2D
 
 @export var MAX_LENGHT := 10
 
+var timer := 0.0
+
 func _process(_delta: float) -> void:
-	var pos = get_parent().global_position
-	add_point(pos)
-	
-	if get_point_count() > MAX_LENGHT:
-		remove_point(0)
+	timer -= _delta
+	if timer <= 0.0:
+		timer = 1.0/60.0
+		var pos = get_parent().global_position
+		add_point(pos)
+		
+		if get_point_count() > MAX_LENGHT:
+			remove_point(0)
