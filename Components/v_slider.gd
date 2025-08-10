b03a89197a666e71d32a82865ba6cc0b301726bb
martypower2064 <martypower2064@gmail.com>
2025-08-10 12:29:39 +0100

    zoom kinda fixed

--- Components/v_slider.gd
+++ Components/v_slider.gd
@@ -1,9 +1,10 @@
 extends VSlider
 @onready var camera : Camera2D = $"../../../Camera2D"
-
+@onready var label : Label =  $"Label"
 
 func _process(_delta: float) -> void:
 	
-	var a  =  abs( -self.value/10)
+	var a  =  abs( self.value/200)
 	if a != 0:
 		camera.zoom = Vector2(a,a)
+	label.text = str(value) + "\n" + str(a)
